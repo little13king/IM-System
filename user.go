6065c68ccff44bd8120af3872f99342f2b971835
钟雨利 <717852291@qq.com>
2024-05-09 16:53:03 +0800

    私聊功能未实现，疑似修改用户名输出问题

--- user.go
+++ user.go
@@ -60,19 +60,19 @@ func (this *User) SendMsg(msg string) {
 
 // DoMessage 用户的消息处理业务
 func (this *User) DoMessage(msg string) {
-	if len(msg) == 3 && msg[:3] == "who" {
+	if msg == "who" {
+		//if len(msg) >= 3 && msg[:3] == "who" {
 		//查询当前用户在线
 		this.server.mapLock.Lock()
 		for _, user := range this.server.OnlineMap {
+			//onlineMsg := fmt.Sprintf("[%s]%s:%s", user.Addr, user.Name, msg)
 			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
 			this.SendMsg(onlineMsg)
 		}
 		this.server.mapLock.Unlock()
-
 	} else if len(msg) > 7 && msg[:7] == "rename|" {
 		//消息格式：rename|张三
 		newName := strings.Split(msg, "|")[1]
-
 		//判断name是否存在
 		_, ok := this.server.OnlineMap[newName]
 		if ok {
@@ -86,6 +86,27 @@ func (this *User) DoMessage(msg string) {
 			this.Name = newName
 			this.SendMsg("已更新用户名为:" + this.Name + "\n")
 		}
+	} else if len(msg) > 4 && msg[:3] == "to|" {
+		//消息格式：to|张三|消息内容
+		//获取对方用户名
+		remoteName := strings.Split(msg, "|")[1]
+		if remoteName == "" {
+			this.SendMsg("消息格式不正确，请使用\"to|张三|消息内容\"格式\n")
+			return
+		}
+		//根据用户名得到对方的user对象
+		remoteUser, ok := this.server.OnlineMap[remoteName]
+		if !ok {
+			this.SendMsg("当前用户不存在\n")
+			return
+		}
+		//获取消息内容，通过对方的user对象将消息发送过去
+		content := strings.Split(msg, "|")[2]
+		if content == "" {
+			this.SendMsg("无消息内容，请重新发送\n")
+			return
+		}
+		remoteUser.SendMsg(this.Name + "对您说：" + content)
 	} else {
 		this.server.BroadCast(this, msg)
 	}
